977e7a30ce8e822197324b231738a6ad601a7cdd
cristian <cristiangrano@gmail.com>
2024-02-27 10:54:19 +0100

    Chat

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import random
+import time
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+st.title("Simple chat")
 
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
